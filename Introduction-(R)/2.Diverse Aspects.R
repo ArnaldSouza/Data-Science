aec5bf46dbfc160292a622e79a94bfcb3e577a49
Arnald Souza <87627213+ArnaldSouza@users.noreply.github.com>
2023-09-01 00:51:16 -0300

    Update 2.Diverse Aspects.R

--- Introduction-(R)/2.Diverse Aspects.R
+++ Introduction-(R)/2.Diverse Aspects.R
@@ -1,13 +1,13 @@
-Directory of work
+#Directory of work
 
 getwd()
 setwd("c:\\dados")
 
-Object Type
+#Object Type
 
 class(iris)
 
-Save an object .
+#Save an object .
 
 #clone
 iristeste = iris
@@ -18,14 +18,14 @@ rm(iristeste)
 #testing the removal
 iristeste
 
-Load 
+#Load 
 
 #load the object of the disk
 load(file="iristeste.Rdata")
 #test
 iristeste
 
-Vector e basic graphic
+#Vector e basic graphic
 
 x = c(12,72,108,168) #c -> combine
 y = c(-1,25,9,4)
