7ebac82011fbf9150ec732f205327ef6f4532059
Arnald Souza <87627213+ArnaldSouza@users.noreply.github.com>
2023-09-01 01:00:20 -0300

    Update and rename 5.Functions.Rmd to 5.Functions.R

--- Introduction-(R)/5.Functions.R
+++ Introduction-(R)/5.Functions.R
@@ -1,4 +1,4 @@
-```{r}
+
 getwd()
 sd # without parentheses shows how the code works and the function code
 head
@@ -6,4 +6,4 @@ head(x=iris, n=2)
 head(iris)
 head(iris,2)
 #head(n=22)  'x' is mandatory, so it is not possible to execute
-```
+
