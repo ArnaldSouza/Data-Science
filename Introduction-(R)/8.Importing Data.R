d10494cc98014bc931eac82b25e23452dbbe1ad6
Arnald Souza <87627213+ArnaldSouza@users.noreply.github.com>
2023-09-01 01:03:54 -0300

    Update and rename 8.Importing Data.Rmd to 8.Importing Data.R

--- Introduction-(R)/8.Importing Data.R
+++ Introduction-(R)/8.Importing Data.R
@@ -1,23 +1,21 @@
 
-```{r}
-
 install.packages("openxlsx")
 library(openxlsx)
-```
-Text
-```{r}
+
+#Text
+
 #text
 x = read.csv(file.choose(),header = TRUE, sep = ",") 
 #header says if the first line of the file is the name of the columns
 x
 x = read.csv("dados/Credit.csv",header = TRUE, sep = ",")
 x
-```
-Excel
-```{r}
+
+#Excel
+
 dados = read.xlsx("C:/Users/Documents/Example.xlsx",sheet= 1)
 #sheet indicates the desired worksheet
 dados
-```
+
 
 
