23e80534737466511645c6f0145ea691cfa4d9a4
Arnald Souza <87627213+ArnaldSouza@users.noreply.github.com>
2023-09-01 00:32:00 -0300

    Update and rename 1.Packages.Rmd to 1.Packages.R

--- Introduction-(R)/1.Packages.R
+++ Introduction-(R)/1.Packages.R
@@ -1,17 +1,15 @@
 
-```{r}
+
 install.packages("e1071")
 library(e1071)
-```
 
-```{r}
+
+
 #unload package from memory
 detach("package:e1071", unload=TRUE)
 #remove/uninstall
 remove.packages("e1071")
-```
 
-```{r}
 plot(iris[,1:4])
-```
+
 
