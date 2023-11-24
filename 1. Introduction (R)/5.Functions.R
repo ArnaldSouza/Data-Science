109ed2ec0485dfd61a0be862b9ad3ebb7ce844a3
Arnald Souza <arnaldsouza472@outlook.com.br>
2023-11-24 00:46:01 -0300

    Changing R to Rmd

--- 1. Introduction (R)/5.Functions.R
+++
@@ -1,9 +0,0 @@
-
-getwd()
-sd # without parentheses shows how the code works and the function code
-head
-head(x=iris, n=2)
-head(iris)
-head(iris,2)
-#head(n=22)  'x' is mandatory, so it is not possible to execute
-
